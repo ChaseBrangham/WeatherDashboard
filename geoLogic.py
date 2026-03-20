3b643377c7e1dde235b0a72e274d900c99b4adb0
ChaseBrangham <chase.t.brangham@gmail.com>
2026-03-19 17:11:45 -0700

    Update geoLogic.py

--- geoLogic.py
+++ geoLogic.py
@@ -1,6 +1,7 @@
+from dotenv import load_dotenv
+import os
 from geopy.geocoders import Nominatim
 import requests
-
 
 
 def getWeatherFromLocation(myLocation: str):
@@ -11,7 +12,11 @@ def getWeatherFromLocation(myLocation: str):
     geolocator = Nominatim(user_agent="WeatherDashboard")
     print("Requesting location:", myLocation)
     location = geolocator.geocode(str(myLocation)) #  i.e. San Diego, California
-    API_KEY = "2f787e4e5b30b11cf92f8350f09e21b9"
+
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+
+
     try:
         lat = location.latitude
         lon = location.longitude
